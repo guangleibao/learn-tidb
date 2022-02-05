705dea96f0217cc5e0569b3f012bfefd96596001
Guanglei BAO <toddbgl@hotmail.com>
2022-02-05 08:32:52 +0800

    01-install-tiup changed

--- general-lab/01-install-tiup.sh
+++ general-lab/01-install-tiup.sh
@@ -1,1 +1,2 @@
-curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh
+#curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh
+source `curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh | grep -i 'Shell profile' | grep -o '/.*rc'`
